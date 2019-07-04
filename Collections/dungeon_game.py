c960b151e5a826616ecc43c6cf1c38d1b6466cfc
Spencer Holladay <spencer.holladay@gmail.com>
2019-07-04 19:36:33 -0400

    adds grid and refactors open screen

--- Collections/dungeon_game.py
+++ Collections/dungeon_game.py
@@ -29,15 +29,14 @@ def get_locations():
 def move_player(player, move):
     x, y = player
     if move == "LEFT":
-      x -= 1
+        x -= 1
     if move == "RIGHT":
-      x += 1
+        x += 1
     if move == "UP":
-      y -= 1
+        y -= 1
     if move == "DOWN":
-      y += 1
+        y += 1
     return x, y
-
 
 
 def get_moves(player):
@@ -54,29 +53,52 @@ def get_moves(player):
     return moves
 
 
-monster, door, player = get_locations()
+def draw_map(player):
+    print(" _" * 5)
+    tile = "|{}"
 
-while True:
-    valid_moves = get_moves(player)
-    clear_screen()
-    print("Welcome to the Dungeon!")
-    print("You're currently in room {}".format(player))
-    print("You can move {}".format(", ".join(valid_moves)))
-    print("Enter QUIT to quit")
+    for cell in CELLS:
+        x, y = cell
+        if x < 4:
+            line_end = ""
+            if cell == player:
+                output = tile.format("X")
+            else:
+                output = tile.format("_")
+        else:
+            line_end = "\n"
+            if cell == player:
+                output = tile.format("X|")
+            else:
+                output = tile.format("_|")
+        print(output, end=line_end)
 
-    move = input("> ")
-    move = move.upper()
 
-    if move == 'QUIT':
-        break
-    if move in valid_moves:
-      player = move_player(player, move)
-    else:
-      print("\n ** Walls are hard! Don't run into them! **\n")
-      continue
+def game_loop():
+    monster, door, player = get_locations()
 
-    # good move? Change player position
-    # bad move? Don't change anything
-    # on the door? They win!
-    # on the monster? They lose!
-    # otherwise loop back around
+    while True:
+        draw_map(player)
+        valid_moves = get_moves(player)
+        
+        print("You're currently in room {}".format(player))
+        print("You can move {}".format(", ".join(valid_moves)))
+        print("Enter QUIT to quit")
+
+        move = input("> ")
+        move = move.upper()
+
+        if move == 'QUIT':
+            break
+        if move in valid_moves:
+            player = move_player(player, move)
+        else:
+            input("\n ** Walls are hard! Don't run into them! **\n")
+        clear_screen()
+
+
+clear_screen()
+print("Welcome to the Dungeon!")
+input = ("Press RETURN to start!")
+clear_screen()
+game_loop()
