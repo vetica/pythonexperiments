a482465b0df27e63ac4620519a6027c3da5d6cd0
Spencer Holladay <spencer.holladay@gmail.com>
2019-07-04 17:08:27 -0400

    refactors indexes when adding items

--- Collections/shopping_2.py
+++ Collections/shopping_2.py
@@ -42,10 +42,8 @@ def show_list():
 
     print("Here's your list:")
 
-    index = 1
-    for item in shopping_list:
+    for index, item in enumerate(shopping_list, start=1):
         print("{}. {}".format(index, item))
-        index += 1
 
     print("-"*10)
 
