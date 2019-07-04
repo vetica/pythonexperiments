610432b08cb75595aa35f998ad297057c3aa1195
Spencer Holladay <spencer.holladay@gmail.com>
2019-07-04 15:31:19 -0400

    adds packer and unpacker functions

--- Collections/luggage.py
+++ Collections/luggage.py
@@ -1,1 +1,11 @@
-git 
+def packer(**kwargs):
+  print(kwargs)
+
+def unpacker(first_name=None, last_name=None):
+  if first_name and last_name:
+    print("Hi {} {}!".format(first_name, last_name))
+  else:
+    print("Hi no name!")
+
+packer(name="Spencer", num=42, spanish_inquisition=None)
+unpacker(**{"last_name": "Holladay", "first_name": "Spencer"})
