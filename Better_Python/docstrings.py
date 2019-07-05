ff1a1256a3fdab6226ac3fe1909b7a4e43ce8d9d
Spencer Holladay <spencer.holladay@gmail.com>
2019-07-04 20:54:57 -0400

    adds docstring

--- Better_Python/docstrings.py
+++ Better_Python/docstrings.py
@@ -1,4 +1,8 @@
 def does_something(arg):
+    """Takes one argument and does something based on type.
+    If arg is a string, returns arg * 3;
+    If arg is an int or float, returns arg + 10
+    """
     if isinstance(arg, (int, float)):
         return arg + 10
     elif isinstance(arg, str):
