e5e4dc35358d7f5776cd4149dc80e4b675e32970
Spencer Holladay <spencer.holladay@gmail.com>
2019-07-06 13:57:02 -0400

    adds regex

--- Regular_Expressions/address_book.py
+++ Regular_Expressions/address_book.py
@@ -4,5 +4,6 @@ names_file = open("names.txt", encoding="utf-8")
 data = names_file.read()
 names_file.close()
 
-print(re.match(r'Love', data))
-print(re.search(r'Kenneth', data))
+# print(re.match(r'Love', data))
+# print(re.search(r'Kenneth', data))
+print(re.search(r'\(\d\d\d\) \d\d\d-\d\d\d\d', data))
